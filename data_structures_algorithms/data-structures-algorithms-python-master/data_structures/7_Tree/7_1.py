3099844cd6327ce5022023b892bf08728c04d34e
antonioRVR <antoniorvrk12@gmail.com>
2025-01-13 21:52:37 +0100

    Update 7_1.py

--- data_structures_algorithms/data-structures-algorithms-python-master/data_structures/7_Tree/7_1.py
+++ data_structures_algorithms/data-structures-algorithms-python-master/data_structures/7_Tree/7_1.py
@@ -18,10 +18,11 @@
 # ```
 
 class TreeNode:
-    def __init__(self, data):
+    def __init__(self, data, designation):
         self.data = data
         self.children = []
         self.parent = None
+        self.designation = designation
 
     def get_level(self):
         level = 0
@@ -32,13 +33,33 @@ class TreeNode:
 
         return level
 
-    def print_tree(self):
-        spaces = ' ' * self.get_level() * 3
-        prefix = spaces + "|__" if self.parent else ""
-        print(prefix + self.data)
-        if self.children:
-            for child in self.children:
-                child.print_tree()
+    def print_tree(self, printmode="name"):
+
+        if printmode == "name":
+            spaces = ' ' * self.get_level() * 3
+            prefix = spaces + "|__" if self.parent else ""
+            print(prefix + self.data)
+            if self.children:
+                for child in self.children:
+                    child.print_tree(printmode)
+        elif printmode == "designation":
+            spaces = ' ' * self.get_level() * 3
+            prefix = spaces + "|__" if self.parent else ""
+            print(prefix + self.designation)
+            if self.children:
+                for child in self.children:
+                    child.print_tree(printmode)
+        elif printmode == "both":
+            spaces = ' ' * self.get_level() * 3
+            prefix = spaces + "|__" if self.parent else ""
+            print(prefix + self.data + " (" + self.designation + ")")
+            if self.children:
+                for child in self.children:
+                    child.print_tree(printmode)
+        else:
+            print("Invalid print mode. Use 'name', 'designation' or 'both'")
+
+    
 
     def add_child(self, child):
         child.parent = self
@@ -66,3 +87,34 @@ def build_product_tree():
     root.add_child(tv)
 
     root.print_tree()
+
+
+def build_management_tree():
+    # CTO Hierarchy
+    infra_head = TreeNode("Vishwa","Infrastructure Head")
+    infra_head.add_child(TreeNode("Dhaval","Cloud Manager"))
+    infra_head.add_child(TreeNode("Abhijit", "App Manager"))
+
+    cto = TreeNode("Chinmay", "CTO")
+    cto.add_child(infra_head)
+    cto.add_child(TreeNode("Aamir", "Application Head"))
+
+    # HR hierarchy
+    hr_head = TreeNode("Gels","HR Head")
+
+    hr_head.add_child(TreeNode("Peter","Recruitment Manager"))
+    hr_head.add_child(TreeNode("Waqas", "Policy Manager"))
+
+    ceo = TreeNode("Nilupul", "CEO")
+    ceo.add_child(cto)
+    ceo.add_child(hr_head)
+
+    return ceo
+
+root_node = build_management_tree()
+
+root_node.print_tree("name")
+print("")
+root_node.print_tree("designation")
+print("")
+root_node.print_tree("both")
